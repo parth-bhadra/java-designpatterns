cf9d1f619924d32cc83c2710f8e6010e0fb56240
parth <parthbhadra14@gmail.com>
2024-08-24 18:57:30 +0530

    refactoring

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,63 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        // how to use builder design pattern
-        // create an object of Builder class
-        // the Builder has methods that will initialize the data members of Builder itself
-        // the build method of the Builder will return a new object of the outer class
 
-        // in this manner, builder design pattern is used to create objects
-
-        // in contrast to creation of objects by constructor
-        // builder design patterns allow to create objects by separating the construction of the object from its representation.
-
-        // when to use
-        // when immutability is the top most priority
-        // because builder design pattern never allows the application to access any data member of the object of the House class
-        // it only gives a fully built House Object through its build method
-
-        // flexibility - avoid multiple constructor creation
-
-        // readability++
-
-        //when objects are simple
-
-
-//        House house = new House.Builder()
-//                .hasRoof(true)
-//                .windows(4)
-//                .build();
-
-//        NotSingleton s1 = new NotSingleton(1, 2);
-//        NotSingleton s2 = new NotSingleton(1, 2);
-//
-//        assert s1.equals(s2) : "Objects not equal";
-
-//        Singleton s1 = new Singleton();
-//        Singleton s1 = new Main.Singleton();
-//        s1.a = 1;
-//        s1.b = 2;
-
-//        Singleton s2 = new Singleton();
-//        Singleton s2 = new Main.Singleton();
-//        s2.a = 3;
-//        s2.b = 4;
-//
-//        System.out.println(s1.a); // 3
-//        System.out.println(s1.b); // 4
-//        System.out.println(s2.a); // 3
-//        System.out.println(s2.b); // 4
-
-//        Singleton s1 = Singleton.getInstance();
-//        s1.a = 1;
-//        s1.b = 2;
-//
-//        Singleton s2 = Singleton.getInstance();
-//        s2.a = 3;
-//        s2.b = 4;
-//
-//        System.out.println(s1.equals(s2));
-//        System.out.println(s1.toString());
-//        System.out.println(s2.toString());
 
         System.out.println("If you see the same value, then singleton was reused (yay!)" + "\n" +
                 "If you see different values, then 2 singletons were created (booo!!)" + "\n\n" +
@@ -109,11 +52,4 @@ public class Main {
             System.out.println(singleton.toString());
         }
     }
-
-
-//    public static class Singleton {
-//        public static int a = 0;
-//        public static int b = 0;
-//
-//    }
 }
--- src/main/java/Singleton.java
+++ src/main/java/Singleton.java
@@ -2,40 +2,16 @@ public class Singleton {
     private static Singleton instance;
     private final String val;
 
-    // when someone tries to create an object
-    // they will use new keyword
-    // when new is used
-    // constructor is called
-    // then it creates the object
-
-    // returning is not possible in a constructor
-    // client should not be allowed to call it then
-    // so make the constructor private
-
     private Singleton(String val) {
         this.val = val;
     }
 
-    // constructor's job is to create the object
-    // so it will be used for sure
-
-    // returning is possible in functions/methods
-    // this will unnecessarily enforce locking even after the object was created once
-    // and slow down the subsequent concurrent calls
-
-//    public synchronized static Singleton getInstance(String val) {
-//        // lock this critical section
-//        if (instance == null) {
-//            synchronized (Singleton.class) {
-//                if (instance == null) instance = new Singleton(val);
-//            }
-//        }
-//        return instance;
-//    }
     public static Singleton getInstance(String val) {
-        // lock this critical section
+        // double check locking
         if (instance == null) {
+            // lock it only for the first time
             synchronized (Singleton.class) {
+                // check again as multiple threads can reach above step
                 if (instance == null) instance = new Singleton(val);
             }
         }
